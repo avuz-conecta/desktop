b838284f3b7ee3d5acec8be6912da908b35d1019
Patrick Rezende <tricio777@gmail.com>
2025-12-30 19:38:18 -0300

    feat: add Avuz Conecta package and blacklist configuration
    - Introduced a new Python script for the Avuz Conecta desktop client, defining build options, dependencies, and packaging details.
    - Added a blacklist file to exclude unnecessary files from the installer package, improving the packaging process.
    
    These changes establish the foundation for the Avuz Conecta application, enhancing its build and deployment configuration.

---
+++ admin/win/craft-blueprints/avuz-conecta/avuz-conecta.py
@@ -0,0 +1,67 @@
+import os
+import info
+from Package.CMakePackageBase import CMakePackageBase
+from CraftCore import CraftCore
+
+
+class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.options.dynamic.registerOption("buildType", "Release")
+
+    def setTargets(self):
+        # Use local source directory
+        self.svnTargets["master"] = ""
+
+        self.description = "Avuz Conecta Desktop Client"
+        self.displayName = "Avuz Conecta"
+        self.webpage = "https://avuz.app"
+
+        self.defaultTarget = "master"
+
+    def setDependencies(self):
+        self.buildDependencies["dev-utils/cmake"] = None
+        self.buildDependencies["dev-utils/nsis"] = None
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+        self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt6/qtwebengine"] = None
+        self.runtimeDependencies["libs/qt6/qtwebsockets"] = None
+        self.runtimeDependencies["libs/qt6/qtmultimedia"] = None
+        self.runtimeDependencies["libs/qt6/qtsvg"] = None
+        self.runtimeDependencies["libs/qt6/qt5compat"] = None
+        self.runtimeDependencies["libs/zlib"] = None
+        self.runtimeDependencies["libs/libp11"] = None
+        self.runtimeDependencies["qt-libs/qtkeychain"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
+        self.runtimeDependencies["libs/openssl"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.subinfo.options.configure.args += [
+            "-DNEXTCLOUD_DEV=OFF",
+            "-DOEM_THEME_CMAKE_FILE=AVUZ.cmake",
+        ]
+
+    def createPackage(self):
+        self.blacklist_file.append(os.path.join(self.blueprintDir(), 'blacklist.txt'))
+
+        # Avuz Conecta branding
+        self.defines["appname"] = "avuzconecta"
+        self.defines["company"] = "Avuz"
+        self.defines["productname"] = "Avuz Conecta"
+        self.defines["display_name"] = "Avuz Conecta"
+        self.defines["version"] = "4.0.3"
+        self.defines["website"] = "https://avuz.app"
+        self.defines["icon"] = os.path.join(self.sourceDir(), "admin/win/nsi/installer.ico")
+        self.defines["icon_png"] = os.path.join(self.sourceDir(), "theme/colored/AvuzConecta-icon.png")
+        self.defines["setupname"] = "AvuzConecta-4.0.3-setup.exe"
+
+        self.applicationExecutable = "avuzconecta"
+
+        self.ignoredPackages += ["binary/mysql"]
+        if not CraftCore.compiler.isLinux:
+            self.ignoredPackages += ["libs/dbus"]
+
+        return super().createPackage()
